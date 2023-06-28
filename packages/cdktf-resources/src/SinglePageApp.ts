ec618f1498005f56fd2ea227da571cd1900fdcb3
Aejay Goehring <aejay.goehring@gmail.com>
2023-06-28 14:37:29 -0500

    Adjust resource keying and unique lambda name.

--- packages/cdktf-resources/src/SinglePageApp.ts
+++ packages/cdktf-resources/src/SinglePageApp.ts
@@ -1,14 +1,22 @@
 import * as aws from "@cdktf/provider-aws";
+import * as random from "@cdktf/provider-random";
 import { Construct } from "constructs";
 import BucketDistribution from "./BucketDistribution";
 import DirectoryBucket from "./DirectoryBucket";
 
-interface SinglePageAppOptions {
+/**
+ * Options for configuring an instance of {@link SinglePageApp}.
+ */
+export interface SinglePageAppOptions {
+  /** The path to the local build directory for the SPA, which should be uploaded to S3. */
   localPath: string;
 
-  certificateArn?: string;
-  domainAliases?: string[];
-  r53zoneId?: string;
+  /** The ARN of the ACM certificate the site should use for TLS. */
+  certificateArn: string;
+  /** A list of domains that the SPA will be served from.  */
+  domainAliases: string[];
+  /** The ID of the Route53 hosted zone to create DNS records in for each domain. */
+  r53zoneId: string;
 }
 
 /**
@@ -19,16 +27,29 @@ export default class SinglePageApp extends Construct {
   bucket: DirectoryBucket;
   distribution: BucketDistribution;
 
+  /**
+   *
+   * @param scope The construct (resource or app) that contains this instance.
+   * @param id The ID of this instance, which should be unique to the scope.
+   * @param options The options for configuring this instance.
+   */
   constructor(scope: Construct, id: string, options: SinglePageAppOptions) {
     super(scope, id);
 
     const { localPath, certificateArn, domainAliases, r53zoneId } = options;
 
+    // Generate a unique ID so we don't trip over ourselves with multiple
+    // SPAs in one AWS account.
+    const functionId = new random.id.Id(this, `rewrite-function-id`, {
+      byteLength: 6,
+      prefix: "rewrite-function-",
+    });
+
     const rewriteFunction = new aws.cloudfrontFunction.CloudfrontFunction(
       this,
       `rewrite-function`,
       {
-        name: `rewrite-function`,
+        name: functionId.b64Url,
         runtime: "cloudfront-js-1.0",
         // TODO: Figure out a better way to build/supply the code for this
         // function so it's not defined inline. A hurdle with this is that
